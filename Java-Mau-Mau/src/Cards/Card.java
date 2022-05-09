e1f1f88171701f271914a8da2d3f462de8238d60
erikr <erikr@DESKTOP-KSU6DBQ.Speedport_W_723V_1_49_000>
2022-05-09 10:11:47 +0200

    added Class Player

--- Java-Mau-Mau/src/Cards/Card.java
+++ Java-Mau-Mau/src/Cards/Card.java
@@ -2,7 +2,7 @@ package Cards;
 
 /**
  * @author Arved Meyer
- * @version 0.2.0
+ * @version 0.3.0
  * 
  * Class to Model Cards of the german Skat-Deck 
  * 
---
+++ Java-Mau-Mau/src/Cards/Player.java
@@ -0,0 +1,13 @@
+package Cards;
+
+/**
+ * @author Arved Meyer
+ * @version 0.1.0
+ * 
+ * Class to model a player in the game 
+ * 
+ */
+
+public class Player {
+
+}
