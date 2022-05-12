daf977b1894c942cc65cb26e03b986bad9c09e35
erikr <erikr@DESKTOP-KSU6DBQ.Speedport_W_723V_1_49_000>
2022-05-12 07:07:34 +0200

    worked on CardStack and moved Card class

--- src/main/java/Cards/CardStack.java
+++ src/main/java/Cards/CardStack.java
@@ -1,12 +1,28 @@
 package Cards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Arved Meyer
- * @version 0.1.0
+ * @version 0.2.0
  * 
- * Class to Model a STACK of CARDS - TODO: Which Collection to use 
+ * Class to Model a STACK of CARDS 
  */
 
 public class CardStack {
+	
+	// We use an Implementation of Interface List to represent our Stack of Cards 
+	private List<Card> cards;
+	
+	// No-Args Constructor - We use ArrayList for the dynamically sized Array 
+	public CardStack() {
+        this.cards = new ArrayList<>();
+    }
+	
+	// Method to create a deck of 32 unique Cards
+	public static CardStack initDeck() {
+		
+	}
 
 }
