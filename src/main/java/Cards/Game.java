8dcd5408ca1d7e17087efac617125c8ae4b81480
erikr <erikr@DESKTOP-KSU6DBQ.Speedport_W_723V_1_49_000>
2022-05-17 08:05:10 +0200

    added TBD-Methods to class Game

--- src/main/java/Cards/Game.java
+++ src/main/java/Cards/Game.java
@@ -1,11 +1,32 @@
 package Cards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Arved Meyer
- * @version 0.1.0
+ * @version 0.2.0
  * @comment Class to Model the logic of a game of Mau-Mau 
  */
 
 public class Game {
+	
+	private List<Player> players; 
+	private Player currentPlayer; 
+	private CardStack mainStack; 
+	// Constructor
+	Game () {
+		players = new ArrayList<>();
+	}
+	
+	// Method to generate instances of class Player and add them to Players 
+	void addPlayers(String[] playerNames) {
+		// TODO: For each element of playerNames, generate a new player-object and add it to players
+	}
+	
+	// Method to create our main Stack of cards 
+	private void createMainStack() {
+		// TODO: Create our mainStack via initDeck and then shuffle it 
+	}
 
 }
