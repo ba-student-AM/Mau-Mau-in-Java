ee98137612d28592e4499275f801044c30537e32
Arved Meyer <s3004612@ba-sachsen.de>
2022-06-30 14:30:28 +0200

    removed unused imports

--- src/main/java/javafx/StartScreenController.java
+++ src/main/java/javafx/StartScreenController.java
@@ -7,11 +7,7 @@
 package javafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
 import java.io.IOException;
-import java.util.Optional;
 
 public class StartScreenController {
   @FXML
