b2ae195b8b08490e36535bd1f0b9734e2db1a8f4
Tobias Hering <tobias.hering96@gmx.de>
2022-07-07 01:49:33 +0200

    prepare the icon and add some comments

--- src/main/java/javafx/App.java
+++ src/main/java/javafx/App.java
@@ -12,6 +12,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,18 +22,26 @@ public class App extends Application {
 
   private static Scene scene;
   private String stage_name = "Java MauMau";
-  // private String icon_path = "";
+  private String icon_file = "icon.png";
   private String default_scene = "startScreen";
   private boolean window_resize = true;
 
   @Override
   public void start(Stage stage) throws IOException {
+    // load the default scene
     scene = new Scene(loadFXML(default_scene));
-    // stage.getIcons().add(new Image(icon_path));
+
+    // set the stage title and icon
     stage.setTitle(stage_name);
     stage.setScene(scene);
+    try {
+      Image icon = new Image(getClass().getResource("/" + icon_file).toString());
+      stage.getIcons().add(icon);
+    } catch (Exception e) {
+      System.out.println("ERROR: Icon not found: " + icon_file);
+    }
 
-    // always show the whole stage
+    // always show the whole content of the stage
     stage.sizeToScene();
     stage.setResizable(window_resize);
     stage.show();
@@ -47,6 +56,7 @@ public class App extends Application {
     System.exit(0);
   }
 
+  // Load a FXML file (switch the scene)
   static void setRoot(String fxml) throws IOException {
     scene.setRoot(loadFXML(fxml));
   }
